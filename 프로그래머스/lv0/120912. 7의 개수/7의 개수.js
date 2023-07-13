29752bee07d5ea778f30cfa1308404011a6ef382
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-07-13 12:26:55 +0900

    [level 0] Title: 7의 개수, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -0,0 +1,12 @@
+function solution(array) {
+    var answer = 0;
+    let a = array.toString()
+    for(let item of a) {
+        if(item.includes(7)){
+            answer += 1
+        } else {
+            answer
+        }
+    }
+    return answer
+}
