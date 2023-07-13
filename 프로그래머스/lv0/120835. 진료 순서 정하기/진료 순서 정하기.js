68c72f769d34697975207c965993b55b8a937486
Jeong <hj08hn@gmail.com>
2023-07-13 11:57:11 +0900

    🔥update Comment
    - 해결한 문제에 코멘트를 답니다
    by @Hah-nna

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,7 +1,8 @@
+// sort가 원본 배열을 바꾸는 메소드인 걸 지금 앎...
+
 function solution(emergency) {
   let sortedArr = [...emergency].sort((x, y) => y - x);
-    // [76, 24, 3]
+  // [76, 24, 3]
   return emergency.map((val) => sortedArr.indexOf(val) + 1);
-    // [3 76 24]
-    
+  // [3 76 24]
 }
--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,23 +1,49 @@
+// for문을 사용해 풀어봄
 function solution(s) {
-    var result = 0;
-    let a = s.split(' ').map((val) => {
-        if(val === 'Z') return 'Z'
-        return Number(val)
-    })
-    
-    for(let i = 0; i < a.length; i++) {
-        if(typeof a[i] === 'number') {
-            result += a[i]
-        } else if(typeof a[i] === 'string') {
-            result -= a[i - 1] 
-        }
+  var result = 0;
+  let a = s.split(" ").map((val) => {
+    if (val === "Z") return "Z";
+    return Number(val);
+  });
+
+  for (let i = 0; i < a.length; i++) {
+    if (typeof a[i] === "number") {
+      result += a[i];
+    } else if (typeof a[i] === "string") {
+      result -= a[i - 1];
     }
-    return result
-    // return a.reduce((acc, cur, curIdx) => {
-    //     if(typeof cur === 'number') {
-    //         acc + cur
-    //     } else if(typeof cur === 'object') {
-    //         acc - cur[curIdx - 1]
-    //     }
-    // }, 0)
+  }
+  return result;
+}
+
+// reduce를 사용해 풀어봄
+// reduce의 세번째 인자로 현재 인덱스가 들어올 수 있는지 처음 앎
+function solution(s) {
+  let a = s.split(" ").map((val) => {
+    if (val === "Z") return "Z";
+    return Number(val);
+  });
+
+  return a.reduce((acc, cur, curIdx) => {
+    if (typeof cur === "number") {
+      return acc + cur;
+    } else if (typeof cur === "string") {
+      return acc - a[curIdx - 1];
+    }
+    return acc;
+  }, 0);
+}
+
+// 다른 사람이 해결한 방법
+// pop을 사용해 a의 요소가 'Z'이면 stack 배열에 담긴 맨 마지막 요소(z 이전의 값)을 뺌
+// 요소가 'Z'가 아니면 숫자로 형변환해서 stack에 push
+function solution(s) {
+  const stack = [];
+
+  s.split(" ").forEach((target) => {
+    if (target === "Z") stack.pop();
+    else stack.push(+target);
+  });
+
+  return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
 }
