820eecd89eb20da1e3e5bf36f2ded550ccfb8ae0
Jeong <hj08hn@gmail.com>
2023-07-13 12:33:50 +0900

    🔥update Comment
    - 해결한 문제에 코멘트를 답니다
    by @Hah-nna

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,17 +1,22 @@
-// function solution(array) {
-//     var answer = 0;
-//     let a = array.toString()
-//     for(let item of a) {
-//         if(item.includes(7)){
-//             answer += 1
-//         } else {
-//             answer
-//         }
-//     }
-//     return answer
-// }
-
+// 내가 해결한 방법
+// 처음에는 두번째 코드처럼 split까지 했다가 그 다음 for문을 돌려 시도했는데 되지 않아
+// includes로 해결함
 function solution(array) {
-    var answer = 0;
-    return array.join('').split('7').length - 1
+  var answer = 0;
+  let a = array.toString();
+  for (let item of a) {
+    if (item.includes(7)) {
+      answer += 1;
+    } else {
+      answer;
+    }
+  }
+  return answer;
+}
+
+// 다른 사람이 해결한 방법
+// ['77717'] -> ["", "", "", "1', ""].length === 5 -> length - 1 = 4
+// length를 사용하는 방법도 생각해보자
+function solution(array) {
+  return array.join("").split("7").length - 1;
 }
