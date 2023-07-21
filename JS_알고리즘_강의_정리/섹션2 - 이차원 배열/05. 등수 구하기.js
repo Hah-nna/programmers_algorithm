1f2eea29a4f61a696c0bb1f01217abd6a109b302
Jeong <hj08hn@gmail.com>
2023-07-21 12:16:09 +0900

    Comment: add comment
    - 주석을 추가합니다

--- JS_알고리즘_강의_정리/섹션2 - 이차원 배열/05. 등수 구하기.js
+++ JS_알고리즘_강의_정리/섹션2 - 이차원 배열/05. 등수 구하기.js
@@ -12,8 +12,11 @@ let arr = [87, 89, 92, 100, 76, 100];
 console.log(solution(arr));
 // 근데 이 코드는 같은 점수일 때 같은 등수로 하는 것을 못함
 
+// 강사님이 푼 코드
 function solution(arr) {
-  let result = Array.from(arr, () => 1);
+  let result = Array.from({ length: 5 }, () => 1);
+  // 길이가 5인 배열을 만들고 요소들을 1로 채워놓음
+
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (arr[j] > arr[i]) result[i]++;
