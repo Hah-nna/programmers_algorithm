7f7cd8c2757089805655334c57107343630e7a05
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-09-12 11:58:48 +0900

    [level 0] Title: 영어가 싫어요, Time: 3.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,41 +1,11 @@
 function solution(numbers) {
-    let numArr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
-    let answer = '';
-    let a = ''
+    let numArr = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     
-    for(let item of numbers) {      
-        a += item
-        if(a === numArr[0]) {
-            answer += '1';
-            a = ''
-        }else if(a === numArr[1]) {
-            answer += '2';
-            a = ''
-        }else if(a === numArr[2]){
-          answer += '3';
-            a = ''
-        }else if(a === numArr[3]) {
-            answer += '4';
-            a = ''
-        }else if(a === numArr[4]){
-          answer += '5';
-            a = ''
-        }else if(a === numArr[5]) {
-          answer += '6';
-            a = ''
-        } else if(a === numArr[6]) {
-            answer += '7';
-            a = ''
-        }else if(a === numArr[7]) {
-          answer += '8';
-            a = ''
-        }else if(a === numArr[8]) {
-            answer += '9';
-            a = ''
-        }else if(a === numArr[9]){
-            answer += '0';
-            a = ''
-        }
+    numArr.forEach((str, idx) => {
+        console.log(str, idx)
+        numbers = numbers.replaceAll(str, idx)
+        console.log(numbers)
+    })
+     return Number(numbers)
     }
- return Number(answer)
-}
+
