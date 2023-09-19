78b137f050253bf242d9f419796c2cb9a418298d
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-09-19 14:59:06 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    var answer = [];
+    let count = 2
+    
+    while(n !== 1){
+        if(n % count === 0) {
+            answer.push(count)
+            n /= count
+        } else {
+        count++    
+        }
+        
+        // n을 count로 나눴을 때 나머지가 0미면
+        // count를 push, 나눈 값을 다시 num에 저장
+        // 만약 나머지가 0이 안 되면 count++하고 다시 2로 나눠보기
+        // 그런데도 나머지가 0이 아니면 종료
+    }
+    return Array.from(new Set(answer))
+}
