b1828273ae85e73c061b764286084b086a009744
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-09-20 16:18:18 +0900

    edit: 소인수분해
    -  다른 방법으로 푼 방법도 넣어봄

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -1,19 +1,37 @@
 function solution(n) {
-    var answer = [];
-    let count = 2
-    
-    while(n !== 1){
-        if(n % count === 0) {
-            answer.push(count)
-            n /= count
-        } else {
-        count++    
-        }
-        
-        // n을 count로 나눴을 때 나머지가 0미면
-        // count를 push, 나눈 값을 다시 num에 저장
-        // 만약 나머지가 0이 안 되면 count++하고 다시 2로 나눠보기
-        // 그런데도 나머지가 0이 아니면 종료
+  var answer = [];
+  let count = 2;
+
+  while (n !== 1) {
+    if (n % count === 0) {
+      answer.push(count);
+      n /= count;
+    } else {
+      count++;
     }
-    return [...new Set(answer)]
+
+    // n을 count로 나눴을 때 나머지가 0미면
+    // count를 push, n은 n / count한 값
+    // 만약 나머지가 0이 안 되면 count++하고 다시 나눠보기
+  }
+  return [...new Set(answer)].sort((x, y) => x - y);
+  // 중복되는 소수가 있을 수 있기 때문에 Set을 사용해 중복된 소수 제거
+
+  // 다른 사람의 풀이 방법
+
+  function solution(n) {
+    let answer = [];
+
+    let i = 2;
+    while (i <= n) {
+      if (n % i === 0) {
+        answer.push(i);
+        n = n / i;
+      } else {
+        i++;
+      }
+    }
+
+    return [...new Set(answer.sort((a, b) => (a > b ? 1 : -1)))];
+  }
 }
