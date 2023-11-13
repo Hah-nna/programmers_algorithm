35d8eb45aedbc9367ab4f5e85d43b88ac84ac1dd
Hah-nna <hj08hn@gmail.com>
2023-11-13 21:16:23 +0900

    update: 등수 매기기

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,16 +1,16 @@
 function solution(score) {
-    const averageArr = score.map((val,idx) => {
-        return (val[0] + val[1]) / 2
-    })
-    
-    const resultArr = Array.from({length: averageArr.length}, () => 1)
-    
-    for(let i = 0; i < averageArr.length; i++) {
-        for(let j = 0; j < averageArr.length; j++) {
-            if(averageArr[i] > averageArr[j]) resultArr[j]++
-        }
+  const averageArr = score.map((val, idx) => {
+    return (val[0] + val[1]) / 2;
+  });
+
+  const resultArr = Array.from({ length: averageArr.length }, () => 1);
+
+  for (let i = 0; i < averageArr.length; i++) {
+    for (let j = 0; j < averageArr.length; j++) {
+      if (averageArr[i] > averageArr[j]) resultArr[j]++;
     }
-        return resultArr
+  }
+  return resultArr;
 }
 
 // [1,1,1,1]
@@ -19,3 +19,10 @@ function solution(score) {
 // i = 1, 70 / [1,2,3,3]
 // i = 2, 55 / [1,2,3,3]
 // i = 3, 65 / [1,2,4,3]
+
+// 다른 사람이 해결한 방법
+function solution(score) {
+  const total = score.map((n) => n[0] + n[1]);
+  const rank = [...total].sort((a, b) => b - a);
+  return total.map((n) => rank.indexOf(n) + 1);
+}
