f0d4c29da9021aaf2922c05f25ec4b6a37dd06d2
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-11-14 21:03:38 +0900

    [level unrated] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,16 +1,20 @@
 function solution(todo_list, finished) {
-    var answer = [];
-    const newTodo = new Map([])
+//     var answer = [];
+//     const newTodo = new Map([])
     
-    for(let i = 0; i < todo_list.length; i++) {
-        newTodo.set(todo_list[i], finished[i])
-        console.log(newTodo.get(todo_list[i]))
-    }
+//     for(let i = 0; i < todo_list.length; i++) {
+//         newTodo.set(todo_list[i], finished[i])
+//         console.log(newTodo.get(todo_list[i]))
+//     }
 
-    for(let i = 0; i < newTodo.size; i++) {
-        if(!newTodo.get(todo_list[i])) {
-            answer.push(todo_list[i])
-        }
-    }
-    return answer
+//     for(let i = 0; i < newTodo.size; i++) {
+//         if(!newTodo.get(todo_list[i])) {
+//             answer.push(todo_list[i])
+//         }
+//     }
+//     return answer
+    
+    return todo_list.filter((val, idx) => {
+        if(!finished[idx]) return todo_list[idx]
+    })
 }
