74accf52a2b398c73c9a2ad7774bbd0653e5e86a
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-11-15 20:45:21 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,8 +1,9 @@
 function solution(my_string, is_suffix) {
- return my_string.indexOf(is_suffix, my_string.length - is_suffix.length) === -1 ? 0 : 1
+ // return my_string.indexOf(is_suffix, my_string.length - is_suffix.length) === -1 ? 0 : 1
 // 6 - 3 = my_string[3] 부터 있는지 검색하고 인덱스 반환 -> 3 -> 1
 // 6 - 3 = 3 my_string[3] 부터 있는지 검색하고 인덱스 반환 -> -1(false) -> 0
 // 6 - 4 = 2 my_string[2] 부터 있는지 검색하고 인덱스 반환 -> -1(false) -> 0
 // 6 - 7 = -1 my_string[-1]인데 문자열에서 사용될때는 0번째부터 검색하고 인덱스 반환 -> -1(false) -> 0
     
+    return my_string.endsWith(is_suffix) ? 1 : 0
 }
