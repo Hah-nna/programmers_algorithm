c5b5a2813a176a6cd4a4f11f3b12e9da23f4f0ff
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-11-21 22:47:59 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,3 +1,4 @@
 function solution(my_string) {
-    return [...my_string].filter((val) => Number(val) || val === '0').sort((x,y) => x - y).map((val) => +val)
+    // return [...my_string].filter((val) => Number(val) || val === '0').sort((x,y) => x - y).map((val) => +val)
+    return my_string.match(/\d/g).sort((x,y) => x - y).map((val) => +val)
 }
