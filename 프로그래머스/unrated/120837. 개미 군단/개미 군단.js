e1dc84aa4abb62f21d7272dd57e2d31f831e9d5f
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-11-21 21:43:14 +0900

    [level unrated] Title: 개미 군단, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,19 @@
+function solution(hp) {
+    
+    var count = 0;
+    while(hp >= 5) {
+        hp -= 5
+        count++
+    }
+    
+    while(hp >= 3) {
+        hp -= 3
+        count++
+    }
+    
+    while(hp > 0) {
+        hp -= 1
+        count++
+    }
+    return count
+}
