e226329a3d8e5e84c81ad091c5e22943be5fdc4d
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2023-11-21 22:36:03 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,5 @@
+function solution(numbers) {
+    var arr = numbers.sort((x,y) => x - y)
+    const len = arr.length
+    return arr[len - 1] * arr[len - 2] > arr[0] * arr[1] ? arr[len - 1] * arr[len - 2] : arr[0] * arr[1]
+}
