0885d81307678791317d621c5235fa76444d2da5
Hah-nna <hj08hn@gmail.com>
2024-06-26 15:59:49 +0900

    add: section 4

--- JS_알고리즘_강의_정리/섹션4. 완전탐색(블루투포스)/05. k번째 큰 수.js
+++ JS_알고리즘_강의_정리/섹션4. 완전탐색(블루투포스)/05. k번째 큰 수.js
@@ -24,3 +24,42 @@ console.log(solution(10, 3, arr));
 // 중복을 피해야하는 거면 Set을 고려하자
 // 앞의 두 문제가 좀 어려워서인지 이번 문제는 아주 쉽다 ㅎㅎㅎㅎ
 // 멘토링과 졸업 선물 문제 다시 복습하자~
+
+// 다시 풀어봄
+// set 생각할 겨를없이 걍 for문 돌려서 해버림;ㅎ
+// 생각하는 과정이 약간 강사님이랑 비슷해져서 뿌듯
+// 중복되는 거는 조건문 걸어서 없애버림
+
+function solution(arr, k) {
+  // 중복되는 걸 조건문을 걸어 피해줌
+  // 첫 번째와 두 번째 고정
+  // 두 번째는 +1
+  // 세 번째는 두 번째의 +1...?
+
+  let a = [];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log("iii", arr[i]);
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      console.log("jj", arr[j + 1]);
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[k] !== arr[i] && arr[k] !== arr[j]) {
+          sum += arr[i] + arr[j] + arr[k];
+          a.push(sum);
+
+          sum = 0;
+        }
+      }
+    }
+  }
+  let answer = [];
+  const b = a.sort((x, y) => y - x);
+  for (let i of b) {
+    if (i !== answer[answer.length - 1]) {
+      answer.push(i);
+    }
+  }
+
+  return answer[k - 1];
+}
