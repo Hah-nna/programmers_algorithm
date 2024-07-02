ffed6c7982b59cc1579c53c6f30f08d68338b8cd
Hah-nna <hj08hn@gmail.com>
2024-07-02 18:00:53 +0900

    add : section 5

--- JS_알고리즘_강의_정리/섹션5 - 효율성(투포인터, 슬라이딩윈도우, 해쉬)/05. 최대 매출(sliding window).js
+++ JS_알고리즘_강의_정리/섹션5 - 효율성(투포인터, 슬라이딩윈도우, 해쉬)/05. 최대 매출(sliding window).js
@@ -31,3 +31,39 @@ function solution(k, arr) {
   }
   return answer;
 }
+
+// 다시 풀어봄
+// 접근법은 괜찮았는데 그걸 코드로 표현 못했음 ㅠㅠ
+// 그래도 슬라이딩 윈도우 기법을 알아서 다행쓰
+
+function solution(k, arr) {
+  let max = 0;
+  let sum = 0;
+
+  // sum에 다음값을 추가하고 아까 더했던 값중 맨 앞에 있는 수를 뺴기
+
+  for (let i = 0; i < k; i++) {
+    sum += arr[i];
+    // 12 15 11 더한값
+  }
+  max = sum;
+  // 38
+
+  for (let i = k; i < arr.length; i++) {
+    sum += arr[i] - arr[i - k];
+    // arr[3] - arr[3-3=0]
+    // 46
+    // arr[4] - arr[4-3=1]
+    // 56
+    // arr[5] - arr[5-3=2]
+    // 55
+    max = Math.max(max, sum);
+    // 46
+    // 56
+    // 56 > 55 = max = 56
+  }
+  return max;
+}
+let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+
+console.log(solution(3, a));
