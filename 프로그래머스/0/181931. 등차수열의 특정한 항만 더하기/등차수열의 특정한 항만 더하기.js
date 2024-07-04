4adb0247936780ebeed6d0ab680f31fade16a6ea
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-04 20:59:34 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,18 @@
+function solution(a, d, included) {
+    var answer = 0;
+    let arr = [a];
+    
+    let sum = a;
+    for(let i = 0; i < included.length - 1; i++) {
+        sum += d
+        arr.push(sum)
+    }
+    
+    const aa = included.filter((val, idx) => {
+        if(val) {
+            return answer += arr[idx]
+        }
+    })
+    
+    return answer
+}
