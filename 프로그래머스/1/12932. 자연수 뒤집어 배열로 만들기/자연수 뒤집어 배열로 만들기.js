ca38cb42f83f30d0844d4a37e1bda9c03d84a178
Hah-nna <hj08hn@gmail.com>
2024-07-09 14:16:56 +0900

    edit : 자연수 뒤집어 배열로 만들기

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,5 +1,19 @@
 function solution(n) {
-    var answer = [];
-    let a = (n+'').split('').map((val) => +val).reverse()
-    return a
+  var answer = [];
+  let a = (n + "")
+    .split("")
+    .map((val) => +val)
+    .reverse();
+  return a;
 }
+
+// 숫자풀이
+
+var arr = [];
+
+do {
+  arr.push(n % 10);
+  n = Math.floor(n / 10);
+} while (n > 0);
+
+return arr;
