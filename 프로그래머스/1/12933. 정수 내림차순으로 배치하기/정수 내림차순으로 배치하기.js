446fbeafda3c97791aa02c6ca89df57b141842c0
Hah-nna <hj08hn@gmail.com>
2024-07-09 14:41:11 +0900

    edit : 정수 내림차순으로 배치하기

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,5 +1,15 @@
 function solution(n) {
-    var answer = 0;
-    let a = (n+"").split('').sort().reverse().join('')
-    return +a
+  var answer = 0;
+  let a = (n + "").split("").sort().reverse().join("");
+  return +a;
 }
+
+// 숫자 풀이
+//숫자가 분명히 더 빠름
+var nums = [];
+do {
+  nums.push(n % 10);
+  n = Math.floor(n / 10);
+} while (n > 0);
+
+return nums.sort((a, b) => b - a).join("") * 1;
