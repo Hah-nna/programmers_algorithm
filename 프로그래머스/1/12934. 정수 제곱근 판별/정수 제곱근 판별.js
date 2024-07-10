57203b8187c9948ab292c003f221884d297c4d30
Hah-nna <hj08hn@gmail.com>
2024-07-10 21:32:27 +0900

    edit : 정수 제곱근 판별 주석추가

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,3 +1,5 @@
 function solution(n) {
-    return Number.isInteger(Math.sqrt(n)) ? Math.pow((Math.sqrt(n) + 1), 2) : -1
+  return Number.isInteger(Math.sqrt(n)) ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
+  // Number.isInteger : 정수인지 판별
+  // Math.pow(n, m) : n을 m만큼 제곱해줌
 }
