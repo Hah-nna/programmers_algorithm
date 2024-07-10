4af69e89d15a5354205fd25939d4baf09357a9e9
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-10 22:49:39 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,16 +1,4 @@
 function solution(numbers) {
-    var answer = 0;
-    let arr = [0,1,2,3,4,5,6,7,8,9]
-
-    numbers.sort()
-    
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] !== numbers[i]) {
-           answer += i
-            numbers.splice(i, 0, i)
-            console.log(numbers)
-            
-        }
-    }
-    return answer
+    let answer = numbers.reduce((acc, cur) => acc + cur) 
+    return 45 - answer
 }
