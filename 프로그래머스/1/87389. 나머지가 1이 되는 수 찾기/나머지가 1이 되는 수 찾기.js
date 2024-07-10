48b7d1f41320a18be0ab4804353c8b2ffa6c3e08
Hah-nna <hj08hn@gmail.com>
2024-07-10 22:46:26 +0900

    edit : 다른 풀이방법

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,11 +1,32 @@
 function solution(n) {
-    var answer = 0
-    
-    for(let i = 1; i <= n; i++) {
-        if(n % i === 1) {
-            answer = i
-            break;
-        } 
+  var answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 1) {
+      answer = i;
+      break;
     }
-    return answer
+  }
+  return answer;
+}
+
+function solution(n) {
+  let i = 1;
+  while (true) {
+    if (n % i === 1) {
+      answer = i;
+      break;
+    } else {
+      i++;
+    }
+  }
+  return answer;
+}
+
+function solution(n, x = 1) {
+  while (x++) {
+    if (n % x === 1) {
+      return x;
+    }
+  }
 }
