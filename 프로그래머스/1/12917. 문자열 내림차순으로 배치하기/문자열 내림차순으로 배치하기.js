c33b50a236cb3c6c1515e46c6666e7f254680041
Hah-nna <hj08hn@gmail.com>
2024-07-11 23:14:12 +0900

    add : 다른 방법

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,16 +1,34 @@
 function solution(s) {
-    // 영문 대문자 : 65 ~ 90, 영문 소문자 : 97 ~ 122
-    let lowerCase = ''
-    let upperCase = ''
-    
-    for(let i of s) {
-        let alphabet = i.charCodeAt()
-        if(alphabet >= 65 && alphabet <= 90) upperCase += i
-        else lowerCase += i
-    }
-    
-    lowerCase = [...lowerCase].sort((x,y) => x > y ? -1 : 1).join('')
-    upperCase = [...upperCase].sort((x,y) => x > y ? -1 : 1).join('')
-    
-    return lowerCase + upperCase
+  // 영문 대문자 : 65 ~ 90, 영문 소문자 : 97 ~ 122
+  let lowerCase = "";
+  let upperCase = "";
+
+  for (let i of s) {
+    let alphabet = i.charCodeAt();
+    if (alphabet >= 65 && alphabet <= 90) upperCase += i;
+    else lowerCase += i;
+  }
+
+  lowerCase = [...lowerCase].sort((x, y) => (x > y ? -1 : 1)).join("");
+  upperCase = [...upperCase].sort((x, y) => (x > y ? -1 : 1)).join("");
+
+  return lowerCase + upperCase;
 }
+
+function solution() {
+  return s
+    .split("")
+    .sort((a, b) => {
+      // * 콜백 함수가 음수를 반환하면, a가 b보다 앞에 정렬됩니다.
+      // * 콜백 함수가 양수를 반환하면, b가 a보다 앞에 정렬됩니다.
+      // * 콜백 함수가 0을 반환하면, a와 b의 상대적인 순서는 변경되지 않습니다.
+      if (a > b) return -1;
+      if (b > a) return 1;
+      return 0;
+    })
+    .join("");
+}
+/**
+ * 참고 블로그
+ * https://medium.com/@su_bak/array-prototype-sort-%EA%B0%80-%EB%8F%99%EC%9E%91%ED%95%98%EB%8A%94-%EB%B0%A9%EC%8B%9D-c3bd2e033b7a
+ */
