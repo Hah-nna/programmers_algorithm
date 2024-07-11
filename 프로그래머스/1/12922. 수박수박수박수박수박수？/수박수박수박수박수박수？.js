4d1ae5dc14d0bd8d69fa77bb4e1d13cb1d38e308
Hah-nna <hj08hn@gmail.com>
2024-07-11 17:20:51 +0900

    edit: 다른 풀이방법

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,15 +1,19 @@
 function solution(n) {
-    var answer = '';
-    // 1 2 3 4
-    // 수 박 수 박
-    // 3 = (2 * 1) + 1
-    // 4 = (2 * 1) + 2
-    // 5 = (2 * 1) + 3
-    // 6 = (2 * 1) + 4
-    // 수, 박 문자열 만들기
-    // i % 2 !== 0 ? answer += 수 : answer += 'qkr'
-    for(let i = 1; i <= n; i++) {
-        i % 2 !== 0 ? answer += '수' : answer += '박'
-    }
-    return answer;
+  var answer = "";
+  // 1 2 3 4
+  // 수 박 수 박
+  // 3 = (2 * 1) + 1
+  // 4 = (2 * 1) + 2
+  // 5 = (2 * 1) + 3
+  // 6 = (2 * 1) + 4
+  // 수, 박 문자열 만들기
+  // i % 2 !== 0 ? answer += 수 : answer += 'qkr'
+  for (let i = 1; i <= n; i++) {
+    i % 2 !== 0 ? (answer += "수") : (answer += "박");
+  }
+  return answer;
+}
+
+function solution(n) {
+  return "수박".repeat(n / 2) + (n % 2 !== 0 ? "수" : "");
 }
