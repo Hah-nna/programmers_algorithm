f272b349492c7c82314d7a45e1b95fd66174b1f8
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-11 16:13:24 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,8 @@
+function solution(pn) {
+    let a = [...pn].slice(pn.length - 4).join('')
+    // let b = pn.split(a).join('')
+    let regexp = /[0-9]/g
+    let b = [...pn].slice(0, pn.length - 4).join('').replaceAll(regexp, '*')
+    return b + a
+
+}
