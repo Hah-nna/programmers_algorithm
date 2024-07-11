ed8e51eff166d3cb46562ffbe4633f55abe59fc2
Hah-nna <hj08hn@gmail.com>
2024-07-11 15:39:37 +0900

    edit : 다른 풀이방법

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,23 @@
 function solution(numbers) {
-    let answer = numbers.reduce((acc, cur) => acc + cur) 
-    return 45 - answer
+  var answer = 0;
+  let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  numbers.sort();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== numbers[i]) {
+      answer += i;
+      numbers.splice(i, 0, i);
+      console.log(numbers);
+    }
+  }
+  return answer;
+}
+
+function solution(numbers) {
+  // 0~9 더한 값 : 45
+  // 45 - number의 모든 요소를 더한 값을 리턴
+
+  let answer = numbers.reduce((acc, cur) => acc + cur, 0);
+  return 45 - answer;
 }
