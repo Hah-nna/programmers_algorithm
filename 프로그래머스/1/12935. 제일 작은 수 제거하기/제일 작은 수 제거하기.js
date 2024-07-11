f0d98c33a5f484bbc0867bcab9723717b84be6c9
Hah-nna <hj08hn@gmail.com>
2024-07-11 15:42:57 +0900

    edit: 다른 풀이방법

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,4 @@
 function solution(arr) {
-<<<<<<< HEAD
   // 돌면서 작은 수 찾음
   // 이거 인덱스를 찾음
   // 삭제함
@@ -13,13 +12,10 @@ function solution(arr) {
   return arr.length !== 1 ? arr.filter((val) => val !== min) : [-1];
 }
 
-=======
-    // 돌면서 작은 수 찾음
-    // 이거 인덱스를 찾음
-    // 삭제함
-    // 삭제하고 남은 어레이를 리턴함
-    // answer !== -1이면 arr.slice(인덱스, 1) : [-1]
-    let min = Math.min(...arr)
-    return arr.length !== 1 ? arr.filter((val) => val !== min) : [-1]
-}
->>>>>>> 68eeb7a7a44c91a6cd77dae20a4d1aceaa0c822c
+// 돌면서 작은 수 찾음
+// 이거 인덱스를 찾음
+// 삭제함
+// 삭제하고 남은 어레이를 리턴함
+// answer !== -1이면 arr.slice(인덱스, 1) : [-1]
+let min = Math.min(...arr);
+return arr.length !== 1 ? arr.filter((val) => val !== min) : [-1];
