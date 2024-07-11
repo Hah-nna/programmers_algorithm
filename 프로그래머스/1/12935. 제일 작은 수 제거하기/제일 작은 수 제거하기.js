68eeb7a7a44c91a6cd77dae20a4d1aceaa0c822c
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-11 15:40:31 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -4,10 +4,6 @@ function solution(arr) {
     // 삭제함
     // 삭제하고 남은 어레이를 리턴함
     // answer !== -1이면 arr.slice(인덱스, 1) : [-1] 
-    let min = arr[0]
-    for(let i of arr) {
-        if(min > i) min = i
-    }
-    // const answer = arr.indexOf(min)
+    let min = Math.min(...arr)
     return arr.length !== 1 ? arr.filter((val) => val !== min) : [-1]
 }
