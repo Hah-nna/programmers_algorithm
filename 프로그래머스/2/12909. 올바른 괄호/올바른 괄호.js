6537c38909b5d1ad54178230bd52804c47a73835
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-16 23:06:14 +0900

    [level 2] Title: 올바른 괄호, Time: 6.03 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,7 +1,8 @@
 function solution(s){
     let arr = []
     // ( <-면 arr에 푸쉬, 아니면 pop
-    // arr.length가 0이면 트루 아니면 펄스
+    // arr.length가 0이면 트루 아니면 false
+    // 근데 '('가 아니고(현재 문자가 ")"라는 뜻) arr.length가 0이라면 짝이 맞지 않으므로 false리턴
     
     for(let i of s) {
         if(i === "(") arr.push(i)
