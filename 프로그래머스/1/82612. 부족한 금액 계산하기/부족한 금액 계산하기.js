6d761276a3af1e20a42e977db38f2a6c6590da1a
Hah-nna <hj08hn@gmail.com>
2024-07-16 14:25:15 +0900

    edit: 다른 풀이방법

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,13 +1,20 @@
 function solution(price, money, count) {
-    var totalPrice = 0;
-    // 3 * 1 = 3
-    // 3 * 2 = 6
-    // 3 * 3 = 9
-    // 3 * 4 = 12 => 30
-    for(let i = 1; i <= count; i++) {
-        totalPrice += (price * i)
-    }
-    // 리턴 30 - 머니
+  var totalPrice = 0;
+  // 3 * 1 = 3
+  // 3 * 2 = 6
+  // 3 * 3 = 9
+  // 3 * 4 = 12 => 30
+  for (let i = 1; i <= count; i++) {
+    totalPrice += price * i;
+  }
+  // 리턴 30 - 머니
 
-    return totalPrice - money > 0 ? totalPrice - money : 0
+  return totalPrice - money > 0 ? totalPrice - money : 0;
+}
+
+// 가우스 공식 사용
+function solution(price, money, count) {
+  const tmp = (price * count * (count + 1)) / 2 - money;
+
+  return tmp > 0 ? tmp : 0;
 }
