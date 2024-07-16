db0615e6fa918633e1a02e0fa8957205abaa9557
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-16 14:24:17 +0900

    [level 1] Title: 직사각형 별찍기, Time: 61.63 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,18 +3,16 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
     let answer = ''
-    // 2중 for of 돌기
-    // * 더하기
-    // 안쪽 포문에 \n 더하기
     
     // a = 5, b = 3
-    for(let i = 0; i < b; i++) {
-        for(let j = 0; j < a; j++) {
-            answer += '*'
-        }
-        answer += '\n'
-    }
+    // for(let i = 0; i < b; i++) {
+    //     for(let j = 0; j < a; j++) {
+    //         answer += '*'
+    //     }
+    //     answer += '\n'
+    // }
+    let aa = '*'.repeat(a) + '\n'
+    let bb = aa.repeat(b)
     
-    
-    console.log(answer)
+    console.log(bb)
 });
