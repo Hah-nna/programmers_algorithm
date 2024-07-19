9d91e9adc639531d8af6d6d3fcf5fae7c3cbba71
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-19 13:31:05 +0900

    [level 1] Title: 예산, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,15 @@
+function solution(d, budget) {
+   d.sort((x,y) => x - y) 
+   let count = 0;
+    for(let i = 0; i < d.length; i++) {
+        budget -= d[i]
+        if(budget > 0) count++
+        else if(budget === 0) {
+            count++
+            return count
+        } else if(budget < 0) return count
+    }
+    
+    
+    return count
+}
