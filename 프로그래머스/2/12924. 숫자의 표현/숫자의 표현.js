a8765a1af0fccc59b27766a1601cbc301bbaaa05
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-22 21:30:50 +0900

    [level 2] Title: 숫자의 표현, Time: 0.33 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,46 +1,34 @@
 function solution(n) {
-    let count = 1;
-    let start = 1
-    let end = 1
-    let sum = 1;
+//     let count = 1;
+//     let start = 1
+//     let end = 1
+//     let sum = 1;
     
-    while(start < n / 2) {
-        // 1
-        if(sum < n) {
-         end++
-        sum += end   
-            // 2 
-            // 1 + 2 = 3
-            // 3 + 3 = 6
-            // 6 + 4 = 10
-            // 10 + 5 = 15
-            
-            // sum = 2
-            // 2 + 3 = 5
-            // 5 + 4 = 9
-            // 9 + 5 = 14
-            // 14 + 6 = 20
-            
-        } else if(sum > n) {
-            start++
-            sum = start
-            end = start
-            // 3
-            // sum = 3
-            // end = 
-        } 
-        if(sum === n) {
-            count++
-            start++
-            sum = start
-            end = start
-            // count = 2
-            // start = 2
-            // sum = 2
-            // end = 3
-        }
-    }
+//     while(start < n / 2) {
+//         if(sum < n) {
+//          end++
+//         sum += end    
+//         } else if(sum > n) {
+//             start++
+//             sum = start
+//             end = start
+//         } 
+//         if(sum === n) {
+//             count++
+//             start++
+//             sum = start
+//             end = start
+//         }
+//     }
     
        
-    return count
+//     return count
+      var answer = 0;
+    for (var i = 1; i <= n; i++) {
+        if ((n % i == 0) && (i % 2 == 1)) {
+            answer++;
+        }
+    }
+    return answer;
+
 }
