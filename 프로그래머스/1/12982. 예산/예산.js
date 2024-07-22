63e52f7b0fcdbc52bf9149da0f18cfa544348297
Hah-nna <hj08hn@gmail.com>
2024-07-22 19:22:04 +0900

    edit: 다른 풀이 추가

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,15 +1,38 @@
+// 내가 푼 방법
 function solution(d, budget) {
-   d.sort((x,y) => x - y) 
-   let count = 0;
-    for(let i = 0; i < d.length; i++) {
-        budget -= d[i]
-        if(budget > 0) count++
-        else if(budget === 0) {
-            count++
-            return count
-        } else if(budget < 0) return count
-    }
-    
-    
-    return count
+  d.sort((x, y) => x - y);
+  let count = 0;
+  for (let i = 0; i < d.length; i++) {
+    budget -= d[i];
+    if (budget > 0) count++;
+    else if (budget === 0) {
+      count++;
+      return count;
+    } else if (budget < 0) return count;
+  }
+
+  return count;
+}
+
+// 남이 푼 방법 (1)
+function solution(d, budget) {
+  return d
+    .sort((a, b) => a - b)
+    .reduce((count, price) => {
+      return count + ((budget -= price) >= 0);
+    }, 0);
+}
+
+// 남이 푼 방법 (2)
+
+function solution(d, budget) {
+  var answer = 0;
+  var index = 0;
+  d.sort((a, b) => a - b);
+
+  while (answer + d[index] <= budget) {
+    answer += d[index];
+    index++;
+  }
+  return index;
 }
