635ada317e57a111c44a44ac4cd1cfb659ef5aa4
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-07-31 22:30:15 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,22 @@
-function solution(arr, commands) {
-    var answer = [];
-    const length = commands.length
+// for문으로 풀어봄
+// function solution(arr, commands) {
+//     var answer = [];
+//     const length = commands.length
     
-    for(let i = 0; i < length; i++) {
-            let cutArr = arr.slice((commands[i][0] - 1), commands[i][1])
-            let sorted = cutArr.sort((x,y) => x - y)
-            answer.push(sorted[commands[i][2] - 1])
-    }
-    return answer
+//     for(let i = 0; i < length; i++) {
+//             let cutArr = arr.slice((commands[i][0] - 1), commands[i][1])
+//             let sorted = cutArr.sort((x,y) => x - y)
+//             answer.push(sorted[commands[i][2] - 1])
+//     }
+//     return answer
+// }
+
+// 디스트럭처링을 사용해 풀기
+function solution(arr, commands) {
+    return commands.map(([start, end, k]) => {
+        const sliced = arr.slice(start - 1, end);
+        const sorted = sliced.sort((a, b) => a - b);
+        return sorted[k - 1];
+    });
 }
+
