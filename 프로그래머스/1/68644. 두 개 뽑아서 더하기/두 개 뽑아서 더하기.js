028edd6bde76f01354cffe00b148f4574bf86ccd
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-08-01 15:20:33 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.83 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,11 +1,23 @@
+// function solution(numbers) {
+//     var answer = [];
+//     for(let i = 0; i <= numbers.length - 2; i++) {
+//         for(let j = i + 1; j < numbers.length; j++) {
+//             let sum = (numbers[i] + numbers[j])
+//             answer.includes(sum) ? '' : answer.push(sum)
+//         }
+//     }
+//     return answer.sort((x,y) => x - y)
+// }
+
 function solution(numbers) {
-    // two pointer
-    var answer = [];
+    var temp = [];
     for(let i = 0; i <= numbers.length - 2; i++) {
         for(let j = i + 1; j < numbers.length; j++) {
             let sum = (numbers[i] + numbers[j])
-            answer.includes(sum) ? '' : answer.push(sum)
+            temp.push(sum)
         }
     }
+    
+    let answer = [...new Set(temp)]
     return answer.sort((x,y) => x - y)
 }
