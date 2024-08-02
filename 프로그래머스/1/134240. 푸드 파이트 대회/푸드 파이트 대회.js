ac5b91e7dfd58aba3433b61e8a809d2790fda075
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-08-02 20:55:13 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,16 +1,28 @@
-function solution(food) {
-    // food = [물, 1번, 2번, 3번 음식]
+// function solution(food) {
+//     // food = [물, 1번, 2번, 3번 음식]
     
-    var answer = '';
+//     var answer = '';
+//     for(let i = 1; i < food.length; i++) {
+//         let a = Math.floor(food[i] / 2)
+//         answer += (i+"").repeat(a)
+//     }
+//     let reverse = [...answer].reverse().join('')
+//     let water = '0'.repeat(food[0])
+    
+//     answer += water
+//     answer += reverse
+//     return answer
+    
+// }
+
+function solution (food) {
+    // 물은 항상 1이라는 걸 지금 봄;;
+    
+    let answer = ''
     for(let i = 1; i < food.length; i++) {
         let a = Math.floor(food[i] / 2)
         answer += (i+"").repeat(a)
     }
-    let reverse = [...answer].reverse().join('')
-    let water = '0'.repeat(food[0])
-    
-    answer += water
-    answer += reverse
+    answer += '0' + [...answer].reverse().join('')
     return answer
-    
 }
