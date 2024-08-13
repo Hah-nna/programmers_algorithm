69339ad9c253cdc0b352d552bb8be4e653cf8b52
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-08-13 15:30:03 +0900

    [level 2] Title: 귤 고르기, Time: 39.10 ms, Memory: 44.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,29 @@
+function solution(k, tangerine) {
+    var count = 0
+    let basket = new Map()
+    
+    for(let i = 0; i < tangerine.length; i++) {
+        if(!basket.has(tangerine[i])) {
+            // map에 없으면
+           basket.set(tangerine[i], 1) 
+        } else {
+            // map에 있다면
+        let value = basket.get(tangerine[i])    
+        basket.set(tangerine[i], value + 1)    
+        } 
+        
+        
+    }
+
+    const sortedCounts = Array.from(basket.values()).sort((a, b) => b - a);
+    
+    // 최소값을 리턴해야하니까 큰 수터 더해봄
+    let sum = 0
+    for(let value of sortedCounts) {
+        sum += value
+        count++
+        if(sum >= k) break;
+    }
+    
+    return count;
+}
