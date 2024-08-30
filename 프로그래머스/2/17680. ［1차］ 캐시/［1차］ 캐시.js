80fb2982250a6d4485a3e07593f7f77c3ae5d6f8
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-08-30 22:22:59 +0900

    [level 2] Title: [1차] 캐시, Time: 71.53 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -3,10 +3,7 @@ function solution(cacheSize, cities) {
     var time = 0;
     const arr = []
     
-    // cacheSize가 0일떄
     if(cacheSize === 0) return cities.length * 5
-    
-    //LRU
     
     for(const city of cities) {
     const lowerCity = city.toLowerCase(); 
@@ -18,19 +15,12 @@ function solution(cacheSize, cities) {
         arr.push(city)
         time += 1
     } else {
-            // 시티가 없으면
       if(arr.length >= cacheSize) {
       arr.shift()   
-      }
-            
+          }
             arr.push(city)
-            time += 5
-            
+            time += 5   
         }
-        
-    
     }
-    
-    
     return time
 }
