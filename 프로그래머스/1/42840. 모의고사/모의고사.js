b84536fcbbbca08e92a00f5a03c2acb715c8bbae
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-03 22:56:24 +0900

    [level 1] Title: 모의고사, Time: 30.89 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,22 @@
 function solution(answers) {
     var answer = [0,0,0];
     
-    const firstAnswer = [1, 2, 3, 4, 5]
-    const secondAnswer = [2, 1, 2, 3, 2, 4, 2, 5]
-    const thirdAnswer = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    const m1 = [1, 2, 3, 4, 5]
+    const m2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    const m3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    const firstLen = firstAnswer.length
-    const secondLen = secondAnswer.length
-    const thirdLen = thirdAnswer.length
+    const firstLen = m1.length
+    const secondLen = m2.length
+    const thirdLen = m3.length
     
     for(let i = 0; i < answers.length; i++) {
         const fIdx = i % firstLen
         const sIdx = i % secondLen
         const tIdx = i % thirdLen
                 
-        if(answers[i] === firstAnswer[fIdx]) answer[0]++
-        if(answers[i] === secondAnswer[sIdx]) answer[1]++
-        if(answers[i] === thirdAnswer[tIdx]) answer[2]++  
+        if(answers[i] === m1[fIdx]) answer[0]++
+        if(answers[i] === m2[sIdx]) answer[1]++
+        if(answers[i] === m3[tIdx]) answer[2]++  
         // console.log(answer[2])
         
         
