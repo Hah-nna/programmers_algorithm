5a8f95806e65b29e0263b813adcb9734c1606cdb
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-19 21:37:31 +0900

    [level 0] Title: n의 배수, Time: 0.02 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.kt
+++ 프로그래머스/0/181937. n의 배수/n의 배수.kt
@@ -1,10 +1,18 @@
+// class Solution {
+//     fun solution(num: Int, n: Int): Int {
+//         var answer: Int = 0
+//         when {
+//             num % n == 0 -> answer = 1
+//             else -> answer = 0
+//         }
+//         return answer
+//     }
+// }
+
 class Solution {
-    fun solution(num: Int, n: Int): Int {
-        var answer: Int = 0
-        when {
-            num % n == 0 -> answer = 1
-            else -> answer = 0
-        }
-        return answer
+    fun solution(num: Int, n : Int): Int {
+        var answer : Int = 0
+        return if(num % n == 0) 1 else 0
     }
 }
+
