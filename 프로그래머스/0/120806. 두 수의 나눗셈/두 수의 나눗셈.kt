f53e20be7e7d4fa1d971d4ed0d1fdfad55fd405b
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-19 21:58:48 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer = (num1.toDouble() / num2) * 1000
+        // println(num1.toDouble())
+        return answer.toInt()
+        
+    }
+}
