b392e2fc5add0b96600c2450f215a61e70aca9b4
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-20 14:46:04 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -1,13 +1,20 @@
 class Solution {
     fun solution(angle: Int): Int {
         var answer = 0;
-        when{
-            angle > 0 && angle < 90 ->  answer = 1
-            angle == 90 ->  answer = 2
-            angle > 90 && angle < 180 -> answer = 3
-            angle >= 180 ->  answer = 4
+        // when{
+        //     angle > 0 && angle < 90 ->  answer = 1
+        //     angle == 90 ->  answer = 2
+        //     angle > 90 && angle < 180 -> answer = 3
+        //     angle >= 180 ->  answer = 4
+        // }
+        // return answer
+       when(angle) {
+            in 1..89 -> answer = 1
+            90 -> answer = 2
+            in 91..179 -> answer = 3
+            else -> answer = 4
         }
-        return answer
+       return answer
     }
 
 }
