c32f66fd007ee780f416df27d15c9438213cce5f
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-20 14:58:52 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.kt
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(array: IntArray, n: Int): Int {
         var answer: Int = 0
-        return array.filter{it == n}.size
+        // return array.filter{it == n}.size
+        return array.count{it == n}
     }
 }
