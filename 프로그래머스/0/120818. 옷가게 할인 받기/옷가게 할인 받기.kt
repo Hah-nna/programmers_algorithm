bdb5c5db5f58a64c036764b85716a611c0b3c975
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-20 16:20:40 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.07 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.kt
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.kt
@@ -1,21 +1,18 @@
-/*
-function solution(price) {
-    if(price < 100000) return price
-    if(price >= 100000 && price < 300000) return Math.trunc(price - (price * 0.05))
-    if(price >= 300000 && price < 500000) return Math.trunc(price - (price * 0.1))
-    if(price >= 500000) return Math.trunc(price - (price * 0.2))
-}
-*/
-
 class Solution {
     fun solution(price: Int): Int {
         var answer: Int = 0
-        answer = when(price) {
-            in 10..99999 -> price
-            in 100000..299999 -> (price - (price * 0.05)).toInt()
-            in 300000..499999 -> (price - (price * 0.1)).toInt()
-            else -> (price - (price * 0.2)).toInt()
-        }
-        return answer
+        var discount = when{
+          price >= 500_000 -> price * 0.8
+          price >= 300_000 -> price * 0.9
+          price >= 100_000 -> price * 0.95
+          else -> price 
+        }.toInt()
+        // answer = when(price) {
+        //     in 10..99999 -> price
+        //     in 100_000..299_999 -> (price - (price * 0.05))
+        //     in 300_000..499_999 -> (price - (price * 0.1))
+        //     else -> (price - (price * 0.2))
+        // }.toInt()
+        return discount
     }
 }
