a98d5136e373c22e6a0bf605ee6c712269363439
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-20 15:26:43 +0900

    [level 0] Title: 중앙값 구하기, Time: 21.10 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.kt
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun solution(array: IntArray): Int {
         var answer: Int = 0
-        array.sort()
-        val middleIdx = array.size / 2
-        return array[middleIdx]
+        val sortedArr = array.sorted()
+        val middleIdx = sortedArr.size / 2
+        return sortedArr[middleIdx]
     }
 }
