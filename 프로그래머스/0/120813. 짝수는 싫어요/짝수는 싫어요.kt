77b406241973d206d4d0bfe32d3239db24cdee0a
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-09-20 15:38:02 +0900

    [level 0] Title: 짝수는 싫어요, Time: 9.20 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.kt
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.kt
@@ -1,6 +1,12 @@
 class Solution {
     fun solution(n: Int): IntArray {
         
-        return (1..n).filter{it % 2 != 0} .toIntArray()
+        // return (1..n).filter{it % 2 != 0} .toIntArray()
+        var answer = mutableListOf<Int>()
+        for(num in 1..n step 2) {
+            answer.add(num)
+        }
+        
+        return answer.toIntArray()
     }
 }
