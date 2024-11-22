a73036cf83679fa0bfc52fbd09343c6263f6802c
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-11-22 23:53:53 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/README.md
+++ 프로그래머스/0/120829. 각도기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 각도기 - 120829 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829) 
 
 ### 성능 요약
 
-메모리: 59.8 MB, 시간: 0.02 ms
+메모리: 60.4 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 20일 14:46:02
+2024년 11월 22일 23:53:51
 
 ### 문제 설명
 
--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -1,20 +1,12 @@
 class Solution {
     fun solution(angle: Int): Int {
-        var answer = 0;
-        // when{
-        //     angle > 0 && angle < 90 ->  answer = 1
-        //     angle == 90 ->  answer = 2
-        //     angle > 90 && angle < 180 -> answer = 3
-        //     angle >= 180 ->  answer = 4
-        // }
-        // return answer
-       when(angle) {
-            in 1..89 -> answer = 1
-            90 -> answer = 2
-            in 91..179 -> answer = 3
-            else -> answer = 4
+      return when(angle) {
+            in 1..89 ->  1
+            90 ->  2
+            in 91..179 ->  3
+            else ->  4
         }
-       return answer
+       
     }
 
 }
