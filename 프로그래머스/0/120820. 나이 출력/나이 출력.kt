cb3613c76640bd415f2cbd5a175b7254eea6c1ed
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2024-11-22 23:47:34 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.kt
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(age: Int): Int {
-        var answer: Int = 0
-        answer = 2022 - age + 1
-        return answer
-    }
+    fun solution(age: Int) = 2022 - age + 1
 }
