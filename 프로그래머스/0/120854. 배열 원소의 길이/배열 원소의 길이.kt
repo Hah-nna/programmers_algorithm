b98c2d8c38f846f64f588a995f025140d4ff924d
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-26 00:26:14 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(strlist: Array<String>) = strlist.map{ it.length }
+    // : IntArray {
+    //     var answer: IntArray = intArrayOf()
+    //     var a = strlist.map{ it.size }
+    //     return a
+    // }
+}
