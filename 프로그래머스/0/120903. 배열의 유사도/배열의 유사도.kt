6b7d09e51aacf9a95a00dd1fbc9d55fc7edc6961
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-26 00:07:14 +0900

    [level 0] Title: 배열의 유사도, Time: 15.99 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.kt
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.kt
@@ -1,12 +1,15 @@
 class Solution {
     fun solution(s1: Array<String>, s2: Array<String>): Int {
-        var answer = 0
-        // 이중포문
-        for(i in s2.indices) {
-            for(j in s1) {
-                if(s2[i] == j) answer++
-            }
-        }
-        return answer
+        // var answer = 0
+        // // 이중포문
+        // for(i in s2.indices) {
+        //     for(j in s1) {
+        //         if(s2[i] == j) answer++
+        //     }
+        // }
+        // return answer
+        
+        // return s1.count { s2.contains(it) }
+        return s1.map(s2::contains).count{ it }
 }
 }
