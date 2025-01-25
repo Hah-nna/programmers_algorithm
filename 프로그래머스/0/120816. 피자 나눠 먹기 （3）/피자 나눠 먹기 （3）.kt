1ec0a75461d45b74fa41a0a0f13285bae7ce7864
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-26 00:23:26 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
@@ -1,8 +1,10 @@
-import kotlin.math.* 
+import kotlin.math.ceil
 
 class Solution {
     fun solution(slice: Int, n: Int): Int {    
-        val expression = n.toFloat() / slice.toFloat()
-        return ceil(expression).toInt()
+        // val expression = n / slice.toDouble()
+        // return ceil(expression).toInt()
+        return (n / slice) + if(n % slice == 0) 0 else 1 
     }
+    // fun solution(slice: Int, n: Int) = ceil( n / slice.toDouble()) 
 }
