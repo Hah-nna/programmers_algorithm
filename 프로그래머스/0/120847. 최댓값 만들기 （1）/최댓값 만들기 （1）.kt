5fe8e904646d10c4eff6bc6f11072b4483c84e14
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-26 11:03:13 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 27.21 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.kt
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.kt
@@ -1,8 +1,8 @@
 class Solution {
-    fun solution(numbers: IntArray): Int {
-        numbers.sort()
-        return numbers[numbers.size - 1] * numbers[numbers.size - 2]
-    }
+    // fun solution(numbers: IntArray): Int {
+    //     numbers.sort()
+    //     return numbers[numbers.size - 1] * numbers[numbers.size - 2]
+    // }
     /**
     * sort() : mutable, 원본 리스트를 변형시켜 정렬시킴  
     * sorted() : immutable, 원본리스트를 변형시키지 않고 정렬된 새로운 리스트 리텉ㄴ
@@ -11,7 +11,7 @@ sortBy()는 리스트 요소가 1 개의 데이터 타입으로 이루어지지 
 
 sortedBy() : Immutable List에서 사용 -> sorted()와 마찬가지로 정렬된 새로운 리스트를 반환
 sortBy() : Mutable List에서 사용 -> Mutable List에 사용 -> sort()와 마찬가지로 자기자신을 정렬
-
-
     */
+    
+    fun solution(numbers: IntArray) = numbers.sorted().let{ it[it.lastIndex] * it[it.lastIndex - 1] }
 }
