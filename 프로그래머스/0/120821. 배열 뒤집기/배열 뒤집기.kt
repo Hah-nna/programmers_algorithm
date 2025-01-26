955bcd544651305a952225d83b83a1ba8f183382
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-26 10:35:54 +0900

    [level 0] Title: 배열 뒤집기, Time: 19.07 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.kt
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -1,3 +1,9 @@
 class Solution {
-    fun solution(list: IntArray) = list.reversedArray()
+    fun solution(list: IntArray) = list.reversed()
+    
+    /*
+    reverse() : 배열을 뒤집은 후 호출한 배열에 적용하는 Unit 함수
+    reversed() : 배열을 뒤집은 후 새로운 객체에 할당하는 List 반환형 함수
+    reversedArray() : 배열을 뒤집은 후 새로운 객체에 할당하는 Array 반환형 함수
+    */
 }
