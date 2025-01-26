4da75d705a0a8dcb13add57718db610a236d42cb
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-26 10:33:11 +0900

    [level 0] Title: 제곱수 판별하기, Time: 4.62 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.kt
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.kt
@@ -2,7 +2,8 @@ class Solution {
     fun solution(n: Int): Int {
         var i: Int = 2
         while(i <= 500000) {
-            if(i * i++ == n) return 1
+            if(i * i == n) return 1
+             i++
         }
         return 2
         
