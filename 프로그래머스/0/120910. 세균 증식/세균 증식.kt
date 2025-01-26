d8f2b8f676df5d04f34a6f2674fcfdc96879cce2
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-26 11:43:14 +0900

    [level 0] Title: 세균 증식, Time: 2.92 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.kt
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.kt
@@ -1,7 +1,11 @@
 import kotlin.math.pow
 
 class Solution {
-    fun solution(n: Int, t: Int): Int {
-        return n * (2.toDouble().pow(t)).toInt() 
-    }
+    // O(log t)
+    // fun solution(n: Int, t: Int): Int {
+    //     return n * (2.toDouble().pow(t)).toInt() 
+    // }
+    
+    // O(t)
+   fun solution(n: Int, t: Int) = (1..t).fold(n) { acc, i -> acc * 2 }
 }
