00d1d44d865a01813e3ecfe439efbd440b25a617
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-27 13:30:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 12.99 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.kt
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.kt
@@ -1,10 +1,13 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer: Int = 0
+//     fun solution(n: Int): Int {
+//         var answer: Int = 0
         
-        for(i in 1..n) {
-            if(n % i == 0) answer++
-        }
-        return answer
-    }
+//         for(i in 1..n) {
+//             if(n % i == 0) answer++
+//         }
+//         return answer
+//     }
+    
+    fun solution(n: Int) = (1..n).count { n % it == 0}
+    
 }
