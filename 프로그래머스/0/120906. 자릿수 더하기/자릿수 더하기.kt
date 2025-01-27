971972f608e2347af9659ca1fde91c408d3fad81
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-27 12:57:59 +0900

    [level 0] Title: 자릿수 더하기, Time: 1.12 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.kt
@@ -1,4 +1,5 @@
 class Solution {
-    fun solution(n: Int) = n.toString().chunked(1).fold(0){ acc, i -> acc + i.toInt()}
+    // fun solution(n: Int) = n.toString().chunked(1).fold(0){ acc, i -> acc + i.toInt()}
+    fun solution(n: Int) = n.toString().sumOf { it.digitToInt() }
     
 }
