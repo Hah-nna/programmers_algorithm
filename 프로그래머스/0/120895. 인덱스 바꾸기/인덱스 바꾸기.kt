5d3dc32d4541f6ab8e0fa6f50dae8cb2f9990bab
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-01-31 12:34:48 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 13.25 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.kt
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.kt
@@ -1,11 +1,17 @@
+import java.util.*
+
 class Solution {
     fun solution(my_string: String, num1: Int, num2: Int): String {
        
-        var charArr = my_string.toCharArray()
+//         var charArr = my_string.toCharArray()
         
-        val temp = charArr[num1]
-        charArr[num1] = charArr[num2]
-        charArr[num2] = temp
-        return charArr.joinToString("")
-    }
+//         val temp = charArr[num1]
+//         charArr[num1] = charArr[num2]
+//         charArr[num2] = temp
+//         return charArr.joinToString("")
+       return my_string.toList().let{
+            Collections.swap(it, num1, num2)
+            it.joinToString("")
+        }
+}
 }
