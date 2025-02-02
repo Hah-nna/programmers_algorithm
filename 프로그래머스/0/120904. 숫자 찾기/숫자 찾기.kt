9624e9d0b74939abc8538e8cbfd8aa9394707795
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-02-03 00:48:31 +0900

    [level 0] Title: 숫자 찾기, Time: 8.17 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.kt
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.kt
@@ -1,7 +1,11 @@
 class Solution {
-    fun solution(num: Int, k: Int): Int {
-        var nums = num.toString()
-        val idx = nums.indexOf(k.toString())
-        return if(idx >= 0) idx + 1 else -1
-    }
+    // fun solution(num: Int, k: Int): Int {
+    //     var nums = num.toString()
+    //     val idx = nums.indexOf(k.toString())
+    //     return if(idx >= 0) idx + 1 else -1
+    // }
+fun solution(num: Int, k: Int):Int {
+    val result = num.toString().indexOf(k.toString())
+    return if(result >=0) result + 1 else result
+}
 }
