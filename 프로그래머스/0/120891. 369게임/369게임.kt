ea3bb12c031f4699d8456f8bb1c953156785e97e
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-02-03 00:14:37 +0900

    [level 0] Title: 369게임, Time: 1.30 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.kt
+++ 프로그래머스/0/120891. 369게임/369게임.kt
@@ -1,3 +1,4 @@
 class Solution {
-    fun solution(order: Int) = order.toString().filter { it == '3' || it == '6' || it == '9' }.length
+    fun solution(order: Int) = order.toString().count { it != '0' && it.digitToInt() % 3 == 0 }
+        // = order.toString().filter { it == '3' || it == '6' || it == '9' }.length
 }
