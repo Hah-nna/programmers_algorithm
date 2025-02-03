d33f160b38b993bdbd6426496efee6ac357f9831
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-02-03 23:12:13 +0900

    [level 0] Title: 중복된 문자 제거, Time: 18.88 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.kt
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.kt
@@ -1,9 +1,12 @@
 class Solution {
-    fun solution(my_string: String): String {
-        var setArr = mutableSetOf<Char>()
+//     fun solution(my_string: String): String {
+//         var setArr = mutableSetOf<Char>()
         
-        my_string.map { setArr.add(it) }
+//         my_string.map { setArr.add(it) }
         
-        return setArr.joinToString("")
-    }
+//         return setArr.joinToString("")
+//     }
+    
+    // fun solution(my_string: String) = my_string.toSet().joinToString("")
+    fun solution(my_string: String) = my_string.toList().distinct().joinToString("")
 }
