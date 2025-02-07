0fe310ce86ead68e0e954c3694500899a4230d16
Jeong hana <116064922+Hah-nna@users.noreply.github.com>
2025-02-08 01:18:43 +0900

    [level 0] Title: A로 B 만들기, Time: 37.21 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.kt
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.kt
@@ -1,16 +1,17 @@
 class Solution {
-    fun solution(before: String, after: String): Int {
-        var answer: Int = 0
-        for(i in before.indices) {
-            var afterStrCount = after.count { it == before[i] }
-            var beforeStrCount = before.count { it == before[i] }
-            if(afterStrCount != beforeStrCount) {
-                answer = 0
-                break
-            } else {
-                answer = 1
-            }
-        }
-        return answer
-    }
+    // fun solution(before: String, after: String): Int {
+    //     var answer: Int = 0
+    //     for(i in before.indices) {
+    //         var afterStrCount = after.count { it == before[i] }
+    //         var beforeStrCount = before.count { it == before[i] }
+    //         if(afterStrCount != beforeStrCount) {
+    //             answer = 0
+    //             break
+    //         } else {
+    //             answer = 1
+    //         }
+    //     }
+    //     return answer
+    // }
+    fun solution(before: String, after: String) = if(before.toList().sorted() == after.toList().sorted()) 1 else 0
 }
